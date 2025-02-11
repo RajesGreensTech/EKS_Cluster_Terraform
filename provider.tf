65b538dd24ac8a3f84214fdead95c8f885854828
RajesGreensTech <eswaranrajeswaran@gmail.com>
2025-02-11 20:05:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,19 +9,19 @@ terraform {
   backend "s3" {
     bucket = "rajesgreentech"
     key    = "vgs/terraform.tfstate"
-    region = "ap-south-1"   
+    region = "us-east-1"   
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 locals {
-  region = "ap-south-1"
+  region = "us-east-1"
   name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
+  azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
